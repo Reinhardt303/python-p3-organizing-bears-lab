d9e44495fd071078047ece79524542886285855a
Thomas <thomasrains01@gmail.com>
2025-01-29 16:26:56 -0800

    submits sql lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, "M", 'brown', "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, "M", 'black', "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, "F", 'brown', "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 11, "F", 'albino', "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 31, "F", 'brown', "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 13, "M", 'black', "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 13, "M", 'white', "angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7, "M", 'black', "calm", 1);
